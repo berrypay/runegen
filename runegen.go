09994c9870b51e48363da4d60191cf808fbbc576
Sallehuddin Abdul Latif <sallehuddin@gmail.com>
2022-09-19 19:34:46 +0800

    ready v1.0.0

---
+++ runegen.go
@@ -0,0 +1,53 @@
+package runegen
+
+import (
+	"math/rand"
+	"time"
+)
+
+var smallCaps = "abcdefghijklmnopqrstuvwxyz"
+var bigCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var symbol = "._#@%&!~|$^*="
+
+func GetRandom(charSet uint8, n int) string {
+	switch charSet {
+	case 1:
+		return generateRandom([]rune(numeric), n)
+	case 2:
+		return generateRandom([]rune(smallCaps), n)
+	case 3:
+		return generateRandom([]rune(smallCaps+numeric), n)
+	case 4:
+		return generateRandom([]rune(bigCaps), n)
+	case 5:
+		return generateRandom([]rune(bigCaps+numeric), n)
+	case 6:
+		return generateRandom([]rune(bigCaps+smallCaps), n)
+	case 7:
+		return generateRandom([]rune(bigCaps+smallCaps+numeric), n)
+	case 10:
+		return generateRandom([]rune(smallCaps+symbol), n)
+	case 11:
+		return generateRandom([]rune(bigCaps+smallCaps+symbol), n)
+	case 12:
+		return generateRandom([]rune(bigCaps+symbol), n)
+	case 13:
+		return generateRandom([]rune(bigCaps+numeric+symbol), n)
+	case 14:
+		return generateRandom([]rune(bigCaps+smallCaps+symbol), n)
+	case 15:
+		return generateRandom([]rune(bigCaps+smallCaps+numeric+symbol), n)
+	default:
+		return generateRandom([]rune(bigCaps+smallCaps+numeric), n)
+	}
+}
+
+func generateRandom(r []rune, n int) string {
+	randomizer := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = r[randomizer.Intn(len(r))]
+	}
+	return string(b)
+}
---
+++ runegen_test.go
@@ -0,0 +1,17 @@
+package runegen
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetRandom(t *testing.T) {
+	for i := 1; i < 10; i++ {
+		got := GetRandom(1, 6)
+		if len(got) != 6 {
+			t.Error("Not enough length generated, test failed.")
+		} else {
+			fmt.Println("Random runes generated: " + got)
+		}
+	}
+}
